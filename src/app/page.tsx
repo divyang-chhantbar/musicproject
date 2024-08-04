54d9bae167a393d2d6209f83b4815cfe17190c5d
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-08-04 14:45:07 +0530

    added a featured courses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from "./components/HeroSection";
+import FeaturedCourses from "./components/FeaturedCourses";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]">
       <HeroSection/>
+      <FeaturedCourses/>
     </main>
   );
 }
