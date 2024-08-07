b026e80dd445929659ea84c7981f858ba25181f4
divyang-chhantbar <divyangchhantbar@gmail.com>
2024-08-07 11:04:51 +0530

    added the sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "./components/HeroSection";
 import FeaturedCourses from "./components/FeaturedCourses";
 import WhyChooseUs from "./components/WhyChooseUs";
 import Testimonials from "./components/Testimonials";
+import UpcomingWebinars from "./components/UpcomingWebinars";
+import FeaturedConstructors from "./components/FeaturedConstructors";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.03]">
@@ -9,6 +11,8 @@ export default function Home() {
       <FeaturedCourses/>
       <WhyChooseUs/>
       <Testimonials/>
+      <UpcomingWebinars/>
+      <FeaturedConstructors/>
     </main>
   );
 }
